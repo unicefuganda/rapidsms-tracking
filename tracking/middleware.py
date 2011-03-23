3d7364d67712ac2980c1cab9fa4f2f8b25e997e6
Mugisha Moses <moses@appfrica.org>
2011-03-23 16:48:07 +0300

    generic view

--- tracking/middleware.py
+++ tracking/middleware.py
@@ -1,5 +1,8 @@
 import datetime
 from tracking.models import UserProfile
+from django.contrib.auth.models import AnonymousUser
+from django.conf import settings
+
 class UserTrackingMiddleware:
     """
     Keeps track of loggedin active users.  Anytime a visitor accesses a valid URL,
@@ -13,28 +16,13 @@ class UserTrackingMiddleware:
         if request.is_ajax(): return
 
         # create some useful variables
-        ip_address = utils.get_ip(request)
         user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
-
-        # see if the user agent is not supposed to be tracked
-        for ua in UntrackedUserAgent.objects.all():
-            # if the keyword is found in the user agent, stop tracking
-            if str(user_agent).find(ua.keyword) != -1:
-                return
 
         if hasattr(request, 'session'):
             # use the current session key if we can
             session_key = request.session.session_key
         else:
-            # otherwise just fake a session key
-            session_key = '%s:%s' % (ip_address, user_agent)
-
-        # don't track media file requests
-        if settings.MEDIA_URL:
-            prefixes.append(settings.MEDIA_URL)
-        if settings.ADMIN_MEDIA_PREFIX:
-            prefixes.append(settings.ADMIN_MEDIA_PREFIX)
-
+            return
 
         # if we get here, the URL needs to be tracked
         # determine what time it is
@@ -43,10 +31,12 @@ class UserTrackingMiddleware:
 
         # for some reason, Visitor.objects.get_or_create was not working here
         user = request.user
-        userprofile = UserProfile.objects.get_or_create(request.user)
+        try:
+            userprofile = UserProfile.objects.get(user=request.user)
+        except UserProfile.DoesNotExist:
+            userprofile=UserProfile.create(userrequest.user)
 
         # determine whether or not the user is logged in
-        user = request.user
         if isinstance(user, AnonymousUser):
             return
 
@@ -55,10 +45,8 @@ class UserTrackingMiddleware:
 
         # if the visitor record is new, or the visitor hasn't been here for
         # at least an hour, update their referrer URL
-        one_hour_ago = now - timedelta(hours=1)
+        one_hour_ago = now - datetime.timedelta(hours=1)
         if not userprofile.last_update or userprofile.last_update <= one_hour_ago:
-            userprofile.referrer = request.META.get('HTTP_REFERER', 'unknown')[:255]
-
             # reset the number of pages they've been to
             userprofile.page_views = 0
             userprofile.session_start = now
@@ -66,4 +54,4 @@ class UserTrackingMiddleware:
         userprofile.url = request.path
         userprofile.page_views += 1
         userprofile.last_update = now
-        visitor.save()
+        userprofile.save()
--- tracking/urls.py
+++ tracking/urls.py
@@ -1,7 +1,22 @@
 from django.conf.urls.defaults import *
-from simple_locations.views import *
+from generic.views import generic
+from django.contrib.auth.decorators import login_required
+from tracking.models import UserProfile
+from generic.sorters import SimpleSorter, TupleSorter
+
 
 urlpatterns = patterns('',
-  # (r'^tracking/edit/(?P<area_id>[0-9]+)/$', edit_location),
+  url(r'^tracking/userlist/$', login_required(generic), {
+      'model':UserProfile,
+      'objects_per_page':10,
+      'partial_row':'tracking/partials/tracked_user_row.html',
+      'base_template':'tracking/tracked_users_base.html',
+      'columns':[('User', True, '', SimpleSorter()),
+                 ('Last Update', True, 'connection__c', SimpleSorter(),),
+                 ('Page Views', True, 'date', SimpleSorter(),),
+                 ('Time On Site', True, 'application', SimpleSorter(),),
+                 ],
+      
+    }),
 
 )
