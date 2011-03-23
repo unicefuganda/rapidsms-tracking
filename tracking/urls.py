cfc433c44cbc3f6d6208009feac2ca54b9ea506e
Mugisha Moses <moses@appfrica.org>
2011-03-23 17:26:36 +0300

    changed model to generic view from UserProfile to User

--- tracking/urls.py
+++ tracking/urls.py
@@ -3,15 +3,19 @@ from generic.views import generic
 from django.contrib.auth.decorators import login_required
 from tracking.models import UserProfile
 from generic.sorters import SimpleSorter, TupleSorter
+from django.contrib.auth.models import User
 
 
 urlpatterns = patterns('',
   url(r'^tracking/userlist/$', login_required(generic), {
-      'model':UserProfile,
+      'model':User,
       'objects_per_page':10,
       'partial_row':'tracking/partials/tracked_user_row.html',
       'base_template':'tracking/tracked_users_base.html',
-      'columns':[('User', True, '', SimpleSorter()),
+      'columns':[
+
+                    ('User', True, '', SimpleSorter()),
+                    ('Group', False, '',SimpleSorter()),
                  ('Last Update', True, 'connection__c', SimpleSorter(),),
                  ('Page Views', True, 'date', SimpleSorter(),),
                  ('Time On Site', True, 'application', SimpleSorter(),),
