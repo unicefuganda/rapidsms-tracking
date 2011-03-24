514f214abd085eed32a94516d816d383006c032d
David McCann <david.a.mccann@gmail.com>
2011-03-24 16:51:46 +0300

    further cleanup

--- tracking/urls.py
+++ tracking/urls.py
@@ -11,17 +11,17 @@ urlpatterns = patterns('',
                            'model':User,
                            'queryset':get_users,
                            'objects_per_page':10,
+                           'selectable':False,
                            'partial_row':'tracking/partials/tracked_user_row.html',
                            'base_template':'tracking/tracked_users_base.html',
                            'columns':[
                                    ('User', True, 'username', SimpleSorter(),),
                                    ('Group', True, 'groups', SimpleSorter(),),
-                                    ('Location', True, 'contact__reporting_location', SimpleSorter(),),
-                                   ('Last Update', True, 'userlog__last_update', SimpleSorter(),),
-                                   ('Page Views', True, 'userlog__page_views', SimpleSorter(),),
-                                   ('Last Page Visited', True, 'userlog__url', SimpleSorter(),),
-                                   ('Average Time On Site', True, 'userlog__time_on_site', SimpleSorter(),),
-                                   ('Average Weekly Visits', True, 'userlog__time_on_site', SimpleSorter(),)
+                                   ('Last Update', False, 'last_update', None,),
+                                   ('Page Views', False, 'page_views', None,),
+                                   ('Last Page Visited', False, '', None,),
+                                   ('Average Visit Time (min)', True, 'avg_time', SimpleSorter(),),
+                                   ('Average Weekly Visits', True, 'avg_weekly', SimpleSorter(),)
                                    ],
 
                            }),
