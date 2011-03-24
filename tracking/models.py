59150f4b8e437db3ba7ceec5fff510256470a340
Mugisha Moses <moses@appfrica.org>
2011-03-24 12:54:39 +0300

    some cleanup

--- tracking/models.py
+++ tracking/models.py
@@ -17,32 +17,14 @@ class UserLog(models.Model):
     referrer=models.CharField(max_length=255,null=True)
     time_on_site = models.PositiveIntegerField(default=0)
     
-    def _timeon_site(self):
-        """
-        Attempts to determine the amount of time a visitor has spent on the
-        site based upon their information that's in the database.
-        """
-        if self.session_start:
-            seconds = (self.last_update - self.session_start).seconds
-
-            hours = seconds / 3600
-            seconds -= hours * 3600
-            minutes = seconds / 60
-            seconds -= minutes * 60
-
-            return u'%i:%02i:%02i' % (hours, minutes, seconds)
-        else:
-            return u'unknown'
-    timeon_site = property(_timeon_site)
-
     def average_time_on_site(self):
         return UserLog.objects.filter(user=self.user).aggregate(Avg('time_on_site')).get('time_on_site__avg',0)
 
-
-    def average_time_onsite(self):
-        pass
     def save(self,*args,**kwargs):
-        self.time_on_site=(self.last_update - self.session_start).seconds
+        try:
+            self.time_on_site=(self.last_update - self.session_start).seconds
+        except:
+            pass
         super(UserLog,self).save(*args,**kwargs)
 
     class Meta:
